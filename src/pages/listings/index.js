207195e93168e7db171ebadb6410c7b1ffc95974
turbowizard <turbowizardry@protonmail.com>
2023-12-15 19:08:44 +1100

    add create button

--- src/pages/listings/index.js
+++ src/pages/listings/index.js
@@ -1,6 +1,6 @@
 
 import { SellerListings } from '@/components/SellerListings';
-
+import Link from 'next/link'
 export default function Index() {
   return (
     <main
@@ -9,7 +9,11 @@ export default function Index() {
       
       
       <div className="max-w-3xl mx-auto w-full items-center justify-between p-8">
-        <h1 className="text-2xl font-bold text-gray-900 mb-4">Your listings</h1>
+        <div className="flex justify-between items-center mb-4">
+          <h1 className="text-2xl font-bold text-gray-900">Your listings</h1>
+          <Link href="/listings/create" className='bg-indigo-500 hover:bg-indigo-600 border-transparent py-2 px-3 text-white focus-visible:outline-indigo-600 rounded font-medium shadow-sm border'>Create Listing</Link>
+        </div>
+        
         <SellerListings />
       </div>
       
