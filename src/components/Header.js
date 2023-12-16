1f726fd3b57e00b0f4f2ebfc5f2957eae5fc0b5e
turbowizard <turbowizardry@protonmail.com>
2023-12-16 13:54:32 +1100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,43 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import Image from "next/image"
 import Link from "next/link"
+import Head from 'next/head'
 
 export function Header() {
   return (
-    <header className="">
-      <nav className="mx-auto flex max-w-7xl items-center justify-between gap-x-6 p-6 lg:px-8" aria-label="Global">
-        <div className="flex">
-          <Link href="/" className="flex space-x-1 items-center">
-            <Image className="w-8 h-8" src="/bellcoin.png" width={64} height={64} />
-            <span className="font-bold text-lg">BELSwap</span>
-          </Link>
-        </div>
- 
-        <div className="flex flex-1 items-center justify-end gap-x-6">
-          <Link href={`https://belscan.io/address/${process.env.NEXT_PUBLIC_BELLCOIN_ADDRESS}`} target="_blank" className="text-sm font-semibold leading-6 text-gray-900">
-            Escrow contract
-          </Link>
+    <>
+      <Head>
+        <title>BELSwap - Buy and Sell $BEL</title>
+      </Head>
+    
+      <header className="">
+        <nav className="mx-auto flex max-w-7xl items-center justify-between gap-x-6 p-6 lg:px-8" aria-label="Global">
+          <div className="flex">
+            <Link href="/" className="flex space-x-1 items-center">
+              <Image className="w-8 h-8" src="/bellcoin.png" width={64} height={64} />
+              <span className="font-bold text-lg">BELSwap</span>
+            </Link>
+          </div>
+  
+          <div className="flex flex-1 items-center justify-end gap-x-6">
+            <Link href={`https://belscan.io/address/${process.env.NEXT_PUBLIC_BELLCOIN_ADDRESS}`} target="_blank" className="text-sm font-semibold leading-6 text-gray-900">
+              Escrow contract
+            </Link>
 
-          <Link href='/faq' className="text-sm font-semibold leading-6 text-gray-900">
-            FAQ
-          </Link>
+            <Link href='/faq' className="text-sm font-semibold leading-6 text-gray-900">
+              FAQ
+            </Link>
 
-          <Link href='/listings' className="text-sm font-semibold leading-6 text-gray-900">
-            Your listings
-          </Link>
+            <Link href='/listings' className="text-sm font-semibold leading-6 text-gray-900">
+              Your listings
+            </Link>
 
-          <ConnectButton />
-        </div>
-       
-      </nav>
+            <ConnectButton />
+          </div>
+        
+        </nav>
 
-    </header>
+      </header>
+    </>
   )
 }
