477b734c977fae67e06fa5d716e5c88f79b5e014
turbowizard <turbowizardry@protonmail.com>
2023-12-17 10:26:57 +1100

    Update create.js

--- src/pages/listings/create.js
+++ src/pages/listings/create.js
@@ -107,11 +107,11 @@ export default function Create() {
             </label>
             <div className="mt-2">
               <input
-                type="number"
+                type="text"
                 name="bellcoinAmount"
                 id="bellcoinAmount"
-                value={bellcoinAmount}
-                onChange={(e) => setBellcoinAmount(e.target.value)}
+                value={bellcoinAmount.replace(/[^.\d]/g, '')}
+                onChange={(e) => setBellcoinAmount(e.target.value.replace(/[^.\d]/g, ''))}
                 className="block w-full rounded-md border-0 px-3 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
